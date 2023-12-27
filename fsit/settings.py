43946c02171a91e5dac7386a70704cd11105a8c1
Sajid Khan <sajidk25@gmail.com>
2023-12-28 00:58:39 +0600

    dev ready

--- fsit/settings.py
+++ fsit/settings.py
@@ -80,28 +80,28 @@ WSGI_APPLICATION = 'fsit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'defaultdb',
-        'USER': 'doadmin',
-        'PASSWORD': 'xrbp1qu7gs3s6r54',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #      'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'defaultdb',
 #         'USER': 'doadmin',
 #         'PASSWORD': 'xrbp1qu7gs3s6r54',
-#         'HOST': 'fsit-service-do-user-8035914-0.b.db.ondigitalocean.com',
-#         'PORT': '25060',
-
+#         'HOST': 'db',
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'defaultdb',
+        'USER': 'doadmin',
+        'PASSWORD': 'xrbp1qu7gs3s6r54',
+        'HOST': 'fsit-service-do-user-8035914-0.b.db.ondigitalocean.com',
+        'PORT': '25060',
+
+    }
+}
 #     #   'default': {
 #     #     'ENGINE': 'django.db.backends.sqlite3',
 #     #     'NAME': BASE_DIR / 'db.sqlite3',
---
+++ fsit/settings_local.py
@@ -0,0 +1,6 @@
+# pull in the normal settings
+from fsit.settings import *
+
+# no debug for us
+DEBUG = True
+ALLOWED_HOSTS = ['*']
