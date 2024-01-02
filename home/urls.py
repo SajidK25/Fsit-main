ba48883c7b131a6400540f1bce3e9933ce73c33c
Franck Alomassor <franck.alomassor@fsitservices.com>
2024-01-02 00:00:04 +0000

    added blogs, url path to urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('services', views.services, name='services'),
+    path('blogs', views.blogs, name='blogs'),
     path('careers', views.careers, name='careers'),
     path('contact', views.contact, name='contact'),
     path('team', views.team, name='team'),
