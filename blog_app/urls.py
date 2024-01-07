86d9ef2ac6bd9e96ff36f72dc2efac4dde2e21bd
Franck Alomassor <franck.alomassor@fsitservices.com>
2024-01-07 14:42:45 +0000

    new changes

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('blogs', views.blogs, name='blogs'),
+    path('<int:pk>/', views.blog, name='blog'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Blog
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def blogs(request):
     blogs = Blog.objects.all()
     return render(request, 'blog_app/blogs.html', {'blogs': blogs})
+
+def blog(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    return render(request, 'blog_app/single-blog.html', {'blog': blog})
