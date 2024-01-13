2fa66b2779ccc65259e277376b0f325496db3e49
Franck Alomassor <franck.alomassor@fsitservices.com>
2024-01-13 15:13:48 +0000

    pagination mods

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import Blog
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def blogs(request):
-    blogs = Blog.objects.all()
+    blog_list = Blog.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(blog_list, 1)
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)
     return render(request, 'blog_app/blogs.html', {'blogs': blogs})
 
 def blog(request, pk):
